971f26209ea3d4ae5326e4590a27fc1a022620a0
fballou <fballou@ucsd.edu>
2022-09-29 13:47:40 -0700

    change

---
+++ WhereAmI.java
@@ -0,0 +1,15 @@
+class WhereAmI {
+    public static void main(String[] args) {
+      System.out.println(System.getProperty("os.name"));
+      System.out.println(System.getProperty("user.name"));
+      System.out.println(System.getProperty("user.home"));
+      System.out.println(System.getProperty("user.dir"));
+      System.out.println("Subtle change");
+      System.out.println("Subtle Change #2");
+      System.out.println("Subtle Change 3");
+    }
+  }
+
+  //purpose of the keygen is sort of like a "Remember Me" aspect for logging in from your OWN device
+  //allows you to not have to use a password when logging in from your own computer
+  //you still need the password when you're on a different device
